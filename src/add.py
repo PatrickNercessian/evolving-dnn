df773b29a588fb173d089040dbceae6679c6a8aa
wmatray <williammatray@gmail.com>
2025-03-28 01:22:08 -0400

    add linear updates

--- src/add.py
+++ src/add.py
@@ -6,14 +6,14 @@ import torch.fx
 from torch.fx.passes.shape_prop import ShapeProp
 import random
 
-def add_linear(graph, reference_node, mutate: str = 'input'):
+def add_linear(graph, reference_node, reference_node_shape: tuple[int, int]):
     """
     Adds a linear layer to the graph after the reference node.
 
     Args:
         graph: The FX graph
         reference_node: The node after which the new node will be inserted
-        mutate: The type of mutation to perform on the new linear layer input: 'input', 'output', or 'both'
+        reference_node_shape: The shape of the reference node
     Returns:
         graph: The modified graph
     """
@@ -22,21 +22,17 @@ def add_linear(graph, reference_node, mutate: str = 'input'):
     
     # Find a unique name for the new node
     name = f"node_{len(nodes)}"
-    while name in graph.graph._modules:
+    # get list of all module names in graph
+    module_names = [node.target for node in graph.graph.nodes if node.op == "call_module"]
+    while name in module_names:
         name = f"node_{len(nodes) + 1}"
 
+
     # Assign random shape to the new linear layer
-    if mutate == 'input':
-        shape = (random.randint(1, 1000), reference_node.shape[1])
-    elif mutate == 'output':
-        shape = (reference_node.shape[0], random.randint(1, 1000))
-    elif mutate == 'both':
-        shape = (random.randint(1, 1000), random.randint(1, 1000))
-    else:
-        raise ValueError("Invalid mutation type")
+    shape = (reference_node_shape[0], random.randint(1, 1000))
 
     # Add the linear layer to the graph
-    graph.add_submodule(name, nn.Linear(shape[1], shape[0]))
+    graph.add_submodule(name, nn.Linear(shape[0], shape[1]))
     
     # Update the graph connections
     new_node = graph.graph.call_module(
@@ -49,7 +45,4 @@ def add_linear(graph, reference_node, mutate: str = 'input'):
     reference_node.replace_all_uses_with(new_node)
     new_node.args = (reference_node,)
 
-    graph.graph.lint()
-    graph.recompile()
-        
     return graph, new_node
