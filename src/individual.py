2f9593e399d49fe297d14113ac3a9f58f6da188f
Patrick Nercessian <pjnercessian@gmail.com>
2025-04-04 17:35:17 -0400

    feat: add hyperparam mutations and crossover

--- src/individual.py
+++ src/individual.py
@@ -3,9 +3,10 @@ from mingpt.utils import CfgNode as CN
 from .individual_graph_module import IndividualGraphModule
 
 class Individual:
-    def __init__(self, graph_module: IndividualGraphModule, train_config: CN):
+    def __init__(self, graph_module: IndividualGraphModule, train_config: CN, id: int):
         self.graph_module = graph_module
         self.train_config = train_config
+        self.id = id
 
     def __str__(self):
         return f"graph_module={self.graph_module}, train_config={self.train_config})"
@@ -14,4 +15,8 @@ class Individual:
         return self.__str__()
 
     def __deepcopy__(self, memo):
-        return Individual(self.graph_module.__deepcopy__(memo), self.train_config.__deepcopy__(memo))
+        return Individual(
+            self.graph_module.__deepcopy__(memo),
+            self.train_config.__deepcopy__(memo),
+            self.id
+        )
