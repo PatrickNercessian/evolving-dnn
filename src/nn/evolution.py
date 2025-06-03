624753dab076d01bed19316fbf97eeba5fffd761
Patrick Nercessian <pjnercessian@gmail.com>
2025-06-03 11:27:13 -0400

    feat: save the individuals to disk

--- src/nn/evolution.py
+++ src/nn/evolution.py
@@ -1,7 +1,9 @@
 import copy
+import json
 import logging
 import os
 
+import torch
 from torch.fx import Graph
 
 from src.evolution import Evolution
@@ -11,12 +13,24 @@ from src.nn.visualization import visualize_graph
 class NeuralNetworkEvolution(Evolution):
     def _log_individuals(self):
         experiment_individuals_path = self.kwargs.get("experiment_individuals_path", None)
-        os.makedirs(experiment_individuals_path, exist_ok=True)
+        if experiment_individuals_path:
+            graphs_path = os.path.join(experiment_individuals_path, "graphs")
+            os.makedirs(graphs_path, exist_ok=True)
+            models_path = os.path.join(experiment_individuals_path, "models")
+            os.makedirs(models_path, exist_ok=True)
+            train_configs_path = os.path.join(experiment_individuals_path, "train_configs")
+            os.makedirs(train_configs_path, exist_ok=True)
         
         for individual in self.population:
-            logging.debug(f"Individual {individual.id} has fitness {individual.fitness} with train config {individual.train_config}")
-            if experiment_individuals_path:
-                visualize_graph(individual.graph_module, "model_graph", f"{experiment_individuals_path}/{individual.id}_graph.svg")
+            try:
+                logging.debug(f"Individual {individual.id} has fitness {individual.fitness} with train config {individual.train_config}")
+                if experiment_individuals_path:
+                    visualize_graph(individual.graph_module, "model_graph", f"{graphs_path}/{individual.id}_graph.svg")
+                    with open(f"{train_configs_path}/{individual.id}_train_config.json", "w") as f:
+                        json.dump(individual.train_config.to_dict(), f)
+                    torch.save(individual.graph_module, f"{models_path}/{individual.id}_model.pt")
+            except Exception:
+                logging.exception(f"Error logging/saving individual {individual.id}")
 
     def _copy_individual(self, individual: NeuralNetworkIndividual) -> NeuralNetworkIndividual:
         child = copy.deepcopy(individual)
